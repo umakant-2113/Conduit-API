fe0377028e0b4d9abb50e8515d92bfb4e59267de
umakant-2113 <lodhiumakant800@gmail.com>
2022-10-26 19:59:14 +0530

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 /* GET home page. */
+
+
 router.get("/", function (req, res, next) {
-  res.json({ message: " welcome to my blog app see all the endpoints on my github  umakant-2113/ConduitApi" });
+  res.json({ message: " welcome to my blog app see all the endpoints on my github  umakant-2113/ConduitApi " });
 });
 module.exports = router;
